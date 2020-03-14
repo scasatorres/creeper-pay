92fc4d709853eae94b8a8dfeb10de3ca3d73b684
Sergio Casatorres <s.casatorres@gmail.com>
2020-03-14 23:51:48 +0100

    feat: Added client-side templates

--- src/router/views/auth.ts
+++ src/router/views/auth.ts
@@ -7,4 +7,8 @@ router.get('/login', (req: Request, res: Response) => {
   return res.render('login');
 });
 
+router.get('/signup', (req: Request, res: Response) => {
+  return res.render('signup');
+});
+
 export { router as authRouter };
