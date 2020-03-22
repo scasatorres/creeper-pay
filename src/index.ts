93a7784c368cace3fd168f09742643f45a6e9b4a
Sergio Casatorres <s.casatorres@gmail.com>
2020-03-22 10:42:29 +0100

    feat: Added https cert reading

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
+import https from 'https';
+import * as fs from 'fs';
 import { app } from './app';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+  });
+} else {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync(process.env.KEY_PATH),
+        cert: fs.readFileSync(process.env.CERT_PATH),
+        ca: fs.readFileSync(process.env.CA_PATH),
+      },
+      app,
+    )
+    .listen(port, () => {
+      console.log(`Server is up and running on port ${port}`);
+    });
+}
